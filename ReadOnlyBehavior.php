84a6c3650fadf12d17444c88500f3b1354ad71fe
David B <david@synatree.com>
2015-02-23 10:21:01 -0500

    First real commit

---
+++ ReadOnlyBehavior.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace synatree\behavior;
+use yii\db\ActiveRecord;
+/**
+ * Read Only Behavior
+ * @property ActiveRecord $owner
+ * @author David Baltusavich <david@synatree.com>
+ */
+abstract class ReadOnlyBehavior extends \yii\base\Behavior
+{
+    private $locked = false;
+	public $latchProperty;
+    /**
+     * Set an attribute if unlocked, or else do nothing.
+     * @inheritdoc
+     */
+    public function __set($param, $value)
+    {
+        if ($this->locked) {
+            return;
+        } else {
+            parent::__set($param, $value);
+        }
+    }
+	
+	public function checkLock($event){
+		if(	$this->owner->__get($this->latchProperty) )
+				$this->locked = true;
+		else
+				$this->locked = false;
+	}
+	
+	public function events()
+	{
+		return [
+			ActiveRecord::EVENT_AFTER_INSERT => 'checkLock',
+			ActiveRecord::EVENT_AFTER_UPDATE => 'checkLock',
+			ActiveRecord::EVENT_AFTER_FIND => 'checkLock'
+		];
+	}
+    
+}
